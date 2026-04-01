57852c18a6b906aac5b694d3bc572fcc4a37489b
xhadan <shadansd1@gmail.com>
2026-04-01 22:28:33 +0530

    update solution for leetcode#445

--- 445. Add Two Numbers II.py
+++ 445. Add Two Numbers II.py
@@ -15,3 +15,18 @@ class Solution:
             # Get values if nodes exist, else use 0
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
+            
+            # New digit calculation
+            val = v1 + v2 + carry
+            carry = val // 10    # e.g., 15 // 10 = 1
+            val = val % 10      # e.g., 15 % 10 = 5
+            
+            # Create new node and move pointer
+            cur.next = ListNode(val)
+            cur = cur.next
+            
+            # Advance input pointers if they exist
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        return dummy.next
